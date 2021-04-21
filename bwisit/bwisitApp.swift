fc161a2262d42af6525d188d4c64bd678ab92ec6
MarvinGM <marvinmahilum091197@gmail.com>
2021-04-21 18:37:13 +0800

    Initial Commit

---
+++ bwisit/bwisitApp.swift
@@ -0,0 +1,17 @@
+//
+//  bwisitApp.swift
+//  bwisit
+//
+//  Created by Amben on 4/21/21.
+//
+
+import SwiftUI
+
+@main
+struct bwisitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
